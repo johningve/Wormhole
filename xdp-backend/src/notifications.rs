f72fbcf92b3c438375886c957b4f46af5ba899c0
John Ingve Olsen <johningveolsen@gmail.com>
2021-07-27 18:53:23 +0200

    Implemented the notification interface

--- xdp-backend/src/notifications.rs
+++ xdp-backend/src/notifications.rs
@@ -1,13 +1,15 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 use zbus::dbus_interface;
 use zvariant_derive::Type;
 
-struct Notifications {}
+pub struct Notifications {}
 
 #[dbus_interface(name = "org.freedesktop.Notifications")]
 impl Notifications {
     fn close_notification(&self, id: u32) {
-        println!("CloseNotification called with id {}", id);
+        println!("CloseNotification called with id {:#?}", id);
     }
 
     fn get_capabilities(&self) -> Vec<String> {
@@ -19,30 +21,53 @@ impl Notifications {
         println!("GetServerInformation called");
         return ServerInformation::get();
     }
+
+    fn notify(&self, notification: Notification) -> u32 {
+        println!("Notify called with notification {:#?}", notification);
+        return 0;
+    }
+
+    #[dbus_interface(signal)]
+    fn notification_closed(&self, id: u32, reason: u32) -> zbus::Result<()>;
+
+    #[dbus_interface(signal)]
+    fn action_invoked(&self, id: u32, action_key: &str) -> zbus::Result<()>;
 }
 
-#[derive(Debug, Type, Serialize, Deserialize)]
-pub struct ServerInformation {
+#[derive(Debug, Type, Serialize)]
+pub struct ServerInformation<'a> {
     /// The product name of the server.
-    pub name: String,
+    pub name: &'a str,
 
     /// The vendor name. For example "KDE," "GNOME," "freedesktop.org" or "Microsoft".
-    pub vendor: String,
+    pub vendor: &'a str,
 
     /// The server's version number.
-    pub version: String,
+    pub version: &'a str,
 
     /// The specification version the server is compliant with.
-    pub spec_version: String,
+    pub spec_version: &'a str,
 }
 
-impl ServerInformation {
-    fn get() -> ServerInformation {
-        return ServerInformation {
-            name: String::from("master"),
-            vendor: String::from("John Ingve Olsen"),
-            version: String::from("0.1"),
-            spec_version: String::from("1.2"),
+impl<'a> ServerInformation<'_> {
+    fn get() -> Self {
+        return Self {
+            name: "master",
+            vendor: "John Ingve Olsen",
+            version: "0.1",
+            spec_version: "1.2",
         };
     }
+}
+
+#[derive(Debug, Type, Deserialize)]
+pub struct Notification<'a> {
+    app_name: &'a str,
+    replaces_id: u32,
+    app_icon: &'a str,
+    summary: &'a str,
+    body: &'a str,
+    actions: Vec<&'a str>,
+    hints: HashMap<&'a str, zvariant::Value<'a>>,
+    expire_timeout: i32,
 }
