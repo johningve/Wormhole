72d529af4cfaa29b886439c7d5a5090f42df74ea
John Ingve Olsen <johningveolsen@gmail.com>
2021-09-08 20:18:08 +0200

    grpc test

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,27 @@
-use std::error::Error;
-
+use futures::future;
+use rpc::notifications::{notifications_client::NotificationsClient, NotifyRequest};
+use std::{convert::TryFrom, error::Error};
+use tonic::transport::{Endpoint, Uri};
+use tower::service_fn;
 use vmsocket::VmSocket;
 
 mod vmsocket;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut socket = VmSocket::connect(6000)?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let channel = Endpoint::try_from("http://[::]:0")?
+        .connect_with_connector(service_fn(|_: Uri| future::ready(VmSocket::connect(7070))))
+        .await?;
+
+    let mut client = NotificationsClient::new(channel);
+
+    let request = tonic::Request::new(NotifyRequest {
+        name: "John Ingve".into(),
+    });
+
+    let response = client.notify(request).await?;
+
+    println!("Response: {:?}", response);
 
     Ok(())
 }
