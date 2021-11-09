0d6e74f11af9b1f27a2e5af6010d1e4385e7c861
John Ingve Olsen <johningveolsen@gmail.com>
2021-11-09 20:47:44 +0100

    dbus socket pipe

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,28 +1,30 @@
-use futures::future;
-use std::{convert::TryFrom, error::Error};
-use tonic::transport::{Endpoint, Uri};
-use tower::service_fn;
-use vmsocket::VmSocket;
-use zbus::ConnectionBuilder;
+use libc::user;
+use std::{error::Error, io::Write, path::Path};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::UnixStream,
+};
 
-mod services;
+use vmsocket::VmSocket;
+use zbus::{Address, ConnectionBuilder};
+
 mod vmsocket;
+
+const CONNECT: &[u8] = b"connect\r\n";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let grpc_channel = Endpoint::try_from("http://[::]:0")?
-        .connect_with_connector(service_fn(|_: Uri| future::ready(VmSocket::connect(7070))))
-        .await?;
-
+    // set up a regular D-Bus connection and initialize services.
     let dbus_connection = ConnectionBuilder::session()?
         .internal_executor(false)
         .build()
         .await?;
 
     // start up a task for the zbus executor to use.
-    let handle = {
+    /* let handle =  */
+    {
         // need to clone connection before moving into async task.
         let dbus_connection = dbus_connection.clone();
         tokio::spawn(async move {
@@ -36,9 +38,47 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .request_name("org.freedesktop.impl.portal.desktop.wsl")
         .await?;
 
-    services::init_all(grpc_channel, &dbus_connection).await?;
+    // services::init_all(grpc_channel, &dbus_connection).await?;
 
-    handle.await?;
+    // handle.await?;
 
-    Ok(())
+    // connect to the bus socket and pipe it to vm socket
+
+    let vm_socket = VmSocket::bind(7070)?;
+
+    let Address::Unix(addr) = Address::session()?;
+
+    let distro_name = std::env::var("WSL_DISTRO_NAME")? + "\r\n";
+    let user_name = whoami::username() + "\r\n";
+    let home_folder = std::env::var("HOME")? + "\r\n";
+
+    let header = [
+        ((distro_name.len() + user_name.len() + home_folder.len()) as u32)
+            .to_le_bytes()
+            .as_ref(),
+        distro_name.as_bytes(),
+        user_name.as_bytes(),
+        home_folder.as_bytes(),
+    ]
+    .concat();
+
+    loop {
+        let mut buffer = [0; 9];
+
+        let mut vm_stream = vm_socket.accept()?;
+        let mut dbus_stream = UnixStream::connect(Path::new(&addr)).await?;
+
+        vm_stream.write_all(&header).await?;
+
+        // FIXME: DOS
+        vm_stream.read_exact(&mut buffer).await?;
+        if buffer != CONNECT {
+            log::warn!("unknown reply");
+            continue;
+        }
+
+        tokio::spawn(async move {
+            tokio::io::copy_bidirectional(&mut dbus_stream, &mut vm_stream).await
+        });
+    }
 }
--- client/src/vmsocket.rs
+++ client/src/vmsocket.rs
@@ -1,11 +1,16 @@
 use nix::{
-    sys::socket::{connect, socket, AddressFamily, SockAddr, SockFlag, SockType},
+    sys::socket::{
+        accept, bind, connect, listen, socket, AddressFamily, SockAddr, SockFlag, SockType,
+    },
     unistd::close,
 };
-use std::{io, net, os::unix::prelude::FromRawFd};
+use std::{
+    io, net,
+    os::unix::prelude::{FromRawFd, RawFd},
+};
 use tokio::net::TcpStream;
 
-pub struct VmSocket;
+pub struct VmSocket(RawFd);
 
 impl VmSocket {
     pub fn connect(port: u32) -> io::Result<TcpStream> {
@@ -24,8 +29,46 @@ impl VmSocket {
             return Err(e.into());
         }
 
-        let stream = unsafe { net::TcpStream::from_raw_fd(fd) };
-        stream.set_nonblocking(true)?;
-        TcpStream::from_std(stream)
+        unsafe { into_tcp_stream(fd) }
     }
+
+    pub fn bind(port: u32) -> io::Result<VmSocket> {
+        let addr = SockAddr::new_vsock(libc::VMADDR_CID_ANY, port);
+
+        let fd = socket(
+            AddressFamily::Vsock,
+            SockType::Stream,
+            SockFlag::empty(),
+            None,
+        )?;
+
+        if let Err(e) = bind(fd, &addr) {
+            let _ = close(fd);
+            return Err(e.into());
+        }
+
+        if let Err(e) = listen(fd, 128) {
+            let _ = close(fd);
+            return Err(e.into());
+        }
+
+        Ok(VmSocket(fd))
+    }
+
+    pub fn accept(&self) -> io::Result<TcpStream> {
+        let fd = accept(self.0)?;
+        unsafe { into_tcp_stream(fd) }
+    }
+}
+
+impl Drop for VmSocket {
+    fn drop(&mut self) {
+        let _ = close(self.0);
+    }
+}
+
+unsafe fn into_tcp_stream(fd: RawFd) -> io::Result<TcpStream> {
+    let stream = net::TcpStream::from_raw_fd(fd);
+    stream.set_nonblocking(true)?;
+    TcpStream::from_std(stream)
 }
