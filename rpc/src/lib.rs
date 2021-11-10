0b690b9a8dedd8616d4f55805846694e84f1781e
John Ingve Olsen <johningveolsen@gmail.com>
2021-11-10 12:09:01 +0100

    remove grpc code

--- rpc/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[allow(non_camel_case_types)]
-pub mod notifications {
-    tonic::include_proto!("notifications");
-}
-
-#[allow(non_camel_case_types)]
-pub mod filechooser {
-    tonic::include_proto!("filechooser");
-}
