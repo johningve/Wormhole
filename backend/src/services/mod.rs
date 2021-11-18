35903f66b952ba7e6b0da0bfff55baa88948d4a1
John Ingve Olsen <johningveolsen@gmail.com>
2021-11-18 19:01:20 +0100

    statusnotifierwatcher impl

--- backend/src/services/mod.rs
+++ backend/src/services/mod.rs
@@ -1,6 +1,9 @@
 use zbus::Connection;
 
-use self::{filechooser::FileChooser, notifications::Notifications};
+use self::{
+    filechooser::FileChooser, notifications::Notifications,
+    statusnotifierwatcher::StatusNotifierWatcher,
+};
 
 pub mod filechooser;
 pub mod notifications;
@@ -11,6 +14,7 @@ pub const PORTAL_PATH: &str = "/org/freedesktop/portal/desktop";
 pub async fn init_all(connection: &Connection, distro: &str) -> zbus::Result<()> {
     FileChooser::init(connection, distro).await?;
     Notifications::init(connection, distro).await?;
+    StatusNotifierWatcher::init(connection, distro).await?;
 
     Ok(())
 }
--- backend/src/services/statusnotifierwatcher.rs
+++ backend/src/services/statusnotifierwatcher.rs
@@ -1,46 +1,74 @@
-use std::{collections::HashSet, sync::Mutex};
-
-use futures::{Future, FutureExt};
-use zbus::{
-    dbus_interface, fdo,
-    names::{BusName, UniqueName},
-    Connection, SignalContext,
+use std::{
+    collections::HashSet,
+    sync::{Arc, Mutex},
 };
-use zvariant::Optional;
 
-#[derive(Default)]
-struct StatusNotifierWatcherData {
-    items: HashSet<String>,
-}
+use futures::FutureExt;
+use zbus::{dbus_interface, fdo, Connection, InterfaceDeref, SignalContext};
+
+const PATH: &str = "/StatusNotifierWatcher";
 
 #[derive(Clone)]
 pub struct StatusNotifierWatcher {
-    data: Mutex<StatusNotifierWatcherData>,
+    items: Arc<Mutex<HashSet<String>>>,
 }
 
 impl StatusNotifierWatcher {
-    pub async fn init(connection: &Connection, distro: &str) -> zbus::Result<()> {
-        let mut watcher = StatusNotifierWatcher {
-            data: Mutex::new(StatusNotifierWatcherData::default()),
+    pub async fn init(connection: &Connection, _distro: &str) -> zbus::Result<()> {
+        let watcher = StatusNotifierWatcher {
+            items: Arc::new(Mutex::new(HashSet::new())),
         };
 
-        let dbus = fdo::DBusProxy::new(connection).await?;
-        dbus.connect_name_owner_changed(|name, old_owner, new_owner| {
-            let watcher = Box::new(watcher.clone());
-            watcher
-                .handle_name_owner_changed(name, old_owner, new_owner)
-                .boxed()
-        })
-        .await?;
+        watcher.handle_name_owner_changed(connection).await?;
+
+        connection.object_server_mut().await.at(PATH, watcher)?;
+
         Ok(())
     }
 
-    async fn handle_name_owner_changed(
-        &self,
-        name: BusName<'_>,
-        old_owner: Optional<UniqueName<'_>>,
-        new_owner: Optional<UniqueName<'_>>,
-    ) {
+    fn insert_item(&self, service: &str) -> bool {
+        let mut items = self.items.lock().unwrap();
+        items.insert(service.to_string())
+    }
+
+    fn remove_item(&self, service: &str) -> bool {
+        let mut items = self.items.lock().unwrap();
+        items.remove(service)
+    }
+
+    async fn handle_name_owner_changed(&self, connection: &Connection) -> zbus::Result<()> {
+        let watcher = self.clone();
+        let connection = connection.clone();
+        let dbus = fdo::DBusProxy::new(&connection).await?;
+        dbus.connect_name_owner_changed(move |name, _old_owner, new_owner| {
+            if new_owner.is_none() {
+                // service was unregistered
+                if watcher.remove_item(name.as_str()) {
+                    let connection = connection.clone(); // rust gets very angry if I don't copy this again.
+                    return async move {
+                        connection
+                        .object_server()
+                        .await
+                        .with(
+                            PATH,
+                            |_iface: InterfaceDeref<'_, StatusNotifierWatcher>, ctx| async move {
+                                StatusNotifierWatcher::status_notifier_item_unregistered(
+                                    &ctx,
+                                    name.as_str(),
+                                )
+                                .await
+                            },
+                        )
+                        .await.unwrap_or_else(|err| log::error!("{}", err));
+                    }
+                    .boxed();
+                }
+            }
+            futures::future::ready(()).boxed()
+        })
+        .await?;
+
+        Ok(())
     }
 }
 
@@ -51,13 +79,19 @@ impl StatusNotifierWatcher {
         #[zbus(signal_context)] ctx: SignalContext<'_>,
         service: &str,
     ) {
+        if self.insert_item(service) {
+            Self::status_notifier_item_registered(&ctx, service)
+                .await
+                .unwrap_or_else(|err| log::error!("{}", err));
+        }
     }
 
     async fn register_status_notifier_host(&self, _service: &str) {}
 
     #[dbus_interface(property)]
-    async fn registered_status_notifier_items(&self) -> Vec<&str> {
-        Vec::new()
+    async fn registered_status_notifier_items(&self) -> Vec<String> {
+        let items = self.items.lock().unwrap();
+        items.iter().map(String::from).collect()
     }
 
     #[dbus_interface(property)]
@@ -74,8 +108,7 @@ impl StatusNotifierWatcher {
     async fn status_notifier_item_registered(
         ctxt: &SignalContext<'_>,
         service: &str,
-    ) -> zbus::Result<()> {
-    }
+    ) -> zbus::Result<()>;
 
     #[dbus_interface(signal)]
     async fn status_notifier_item_unregistered(
